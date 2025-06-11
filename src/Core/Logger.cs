4cb22e09ce6352860c6843903d04fc304aa6d9d2
Midry <115441271+midry3125@users.noreply.github.com>
2025-06-11 23:37:49 +0900

    ネームプレートの方をメッセージウィンドウより手前にした

--- src/Core/Logger.cs
+++ src/Core/Logger.cs
@@ -4,16 +4,45 @@ namespace EmeralEngine.Core
 {
     public class Logger
     {
-        private const string FORMAT = "[{0}] ----------\r\n{1}\r\n----------------\r\n";
+        private const string FORMAT = "[{0}] [{1}]: {2}\r\n";
         private string file;
         public Logger(Managers m)
         {
-            file = Path.Combine(m.ProjectManager.ActualProjectDir, ".log");
+            file = Path.Combine(m.ProjectManager.ActualProjectDir, "log.txt");
+            Info("Start ----------");
         }
 
-        public void Write(string msg)
+        public void Write(string msg, int level)
         {
-            File.AppendAllText(file, string.Format(FORMAT, DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), msg));
+            File.AppendAllText(file, string.Format(FORMAT, DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), LoggingLevel.Get(level), msg));
+        }
+
+        public void Info(string msg)
+        {
+            Write(msg, LoggingLevel.INFO);
+        }
+
+        public void Warning(string msg)
+        {
+            Write(msg, LoggingLevel.WARNING);
+        }
+
+        public void Error(string msg)
+        {
+            Write(msg, LoggingLevel.ERROR);
+        }
+    }
+
+    public struct LoggingLevel
+    {
+        public const int INFO = 1;
+        public const int WARNING = 2;
+        public const int ERROR = 3;
+        public static Dictionary<int, string> _LoggingLevel = new() { {INFO, "Info" }, { WARNING, "Warning" }, { ERROR, "Error"} };
+
+        public static string Get(int n)
+        {
+            return _LoggingLevel[n];
         }
     }
 }
